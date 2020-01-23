81acbf7f007ecd37704d7a0ff3a2a612df97b60b
yizharGoldstein <izhargo@gmail.com>
2020-01-23 02:52:35 +0200

    compare_html_formsorms

--- storage.py
+++ storage.py
@@ -4,7 +4,14 @@ BUCKET_NAME = 'latest-html-form-apple'
 
 s3_client = boto3.client('s3')
 
-# client.list_objects_v2(Bucket='latest-html-form-apple').get('Content')
+
+def check_response(response):
+    if 200 <= response < 300:
+        return True
+    else:
+        return False
+
+
 def init_storage():
     return s3_client.list_objects_v2(Bucket=BUCKET_NAME)['KeyCount']
 
@@ -18,27 +25,30 @@ def check_latest_html_form(html_dict):
 def save_key_value(html_dict):
     storage_response = s3_client.put_object(Body=html_dict['html_form_binary'], Bucket=BUCKET_NAME,
                                             Key=html_dict['signature'])
-    if storage_response == 200:
-        return True
-    else:
-        return False
+    response = storage_response['ResponseMetadata']['HTTPStatusCode']
+    return check_response(response)
+
+
+def delete_current_key(key):
+    storage_response = s3_client.delete_object(Bucket=BUCKET_NAME, Key=key)
+    response = storage_response['ResponseMetadata']['HTTPStatusCode']
+    return check_response(response)
 
 
 def compare_html_forms(html_dict):
     result = True
     obj = s3_client.list_objects_v2(Bucket=BUCKET_NAME)
     current_key = obj['Contents'][0]['Key']
-    data = client.get_object(Bucket='latest-html-form-apple', Key=current_key)
+    data = client.get_object(Bucket=BUCKET_NAME, Key=current_key)
     binary_content = data['Body'].read()
     if html_dict['html_form_binary'] != binary_content:
-        delete_current_key(current_key)
-        result = save_key_value(html_dict)
+        deletion_success = delete_current_key(current_key)
+        if deletion_success:
+            result = save_key_value(html_dict)
+        else:
+            result = False
     return result
 
-
-client.put_object(Body=some_binary_data, Bucket='latest-html-form-apple', Key='test_some.txt')
-
-storage_response.get('ResponseMetadata').get('HTTPStatusCode')
 
 STORAGE_FUNC = {
     0: save_key_value,
